salmanarshad321/framework

commit 9e98711e5ae9601b0ccbe9a205ff5598d0660439

Clean up unneeded line.

src/Illuminate/Auth/RequestGuard.php
@@ -51,9 +51,7 @@ class RequestGuard implements Guard
             return $this->user;
         }
 
-        $user = call_user_func($this->callback, $this->request);
-
-        return $this->user = $user;
+        $this->user = call_user_func($this->callback, $this->request);
     }
 
     /**

commit 1024fc80cc0b8121cb2f904b1226efa4c9ab94c6

Fix RequestGuard::user() returning void
Broken since 9e98711e5ae9601b0ccbe9a205ff5598d0660439

src/Illuminate/Auth/RequestGuard.php
@@ -51,7 +51,7 @@ class RequestGuard implements Guard
             return $this->user;
         }
 
-        $this->user = call_user_func($this->callback, $this->request);
+        return $this->user = call_user_func($this->callback, $this->request);
     }
 
     /**